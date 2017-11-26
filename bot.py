0f685d5da25fb208cff237b305460e20a0019a6c
dmitry <dgaponov99@mail.ru>
2017-11-26 23:15:08 +0300

    pfff

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import logging
 
 API_TOKEN = '442370610:AAHZ0eYC7Bx9Qsm9qNIaJJgK-LKUgmxjUj4'
 
-WEBHOOK_HOST = '<ip/host where the bot is running>'
+WEBHOOK_HOST = 'boiling-badlands-53405.herokuapp.com'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
