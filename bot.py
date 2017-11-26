d626c28369221b5192259bec36d7579b0c03a90f
dmitry <dgaponov99@mail.ru>
2017-11-26 23:42:56 +0300

    pfff

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import telebot
 import os
 from flask import Flask, request
 
-bot = telebot.TeleBot('<api_token>')
+import ConstantValues
+
+bot = telebot.TeleBot(ConstantValues.TOKEN)
 
 server = Flask(__name__)
 
@@ -22,7 +24,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://boiling-badlands-53405.herokuapp.com/")
+    bot.set_webhook(url="https://boiling-badlands-53405.herokuapp.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
