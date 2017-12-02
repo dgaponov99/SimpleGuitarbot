7301eda5309281b0b47410603e4f58e94ac77a11
dmitry <dgaponov99@mail.ru>
2017-12-03 00:46:43 +0300

    try change

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import telebot
 import os
 from flask import Flask, request
-import Config
+import config
 
-bot = telebot.TeleBot(Config.TOKEN)  # Создание бота как объекта
+bot = telebot.TeleBot(config.TOKEN)  # Создание бота как объекта
 
 server = Flask(__name__)  # Создание сервера
 
 
 # Прием сообщений
-@server.route('/' + Config.TOKEN, methods=['POST'])
+@server.route('/' + config.TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
@@ -19,8 +19,12 @@ def get_message():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=Config.URL)
+    bot.set_webhook(url=config.URL)
     return "!", 200
+
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  # Запуск сервера
+server = Flask(__name__)  # Наверно это нужно, чтобы Heroku не засыпал (работает и без этой строки)
 
 
 # Комманда "/start"
@@ -38,7 +42,3 @@ def send_chords(message):
         image.close()
     except FileNotFoundError:
         bot.send_message(message.chat.id, 'В нашей базе нет такого аккорда')
-
-
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  # Запуск сервера
-server = Flask(__name__)  # Наверно это нужно, чтобы Heroku не засыпал
