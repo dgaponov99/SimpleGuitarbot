2a2d7bcf103a0a8ae0fc9709b642b1c6179dc4af
dmitry <dgaponov99@mail.ru>
2017-12-02 23:57:42 +0300

    add chords

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import telebot
 import os
 from flask import Flask, request
-
 import Config
 
 bot = telebot.TeleBot(Config.TOKEN)
@@ -27,9 +26,14 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(content_types=["text"])
+def send_photo(message):
+    try:
+        image = open('res/chords' + message.text.upper + '.jpg', 'rb')
+        bot.send_photo(message.chat.id, image)
+        image.close()
+    except FileNotFoundError:
+        bot.send_message(message.chat.id, 'В нашей базе нет такого аккорда')
 
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
