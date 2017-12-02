e232161f7c5130cd2ca8fc73b46ec7b1f223cc89
dmitry <dgaponov99@mail.ru>
2017-12-03 00:42:45 +0300

    add comments

--- bot.py
+++ bot.py
@@ -3,17 +3,19 @@ import os
 from flask import Flask, request
 import Config
 
-bot = telebot.TeleBot(Config.TOKEN)
+bot = telebot.TeleBot(Config.TOKEN)  # Создание бота как объекта
 
-server = Flask(__name__)
+server = Flask(__name__)  # Создание сервера
 
 
+# Прием сообщений
 @server.route('/' + Config.TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
+# Установка вебхуков
 @server.route("/")
 def webhook():
     bot.remove_webhook()
@@ -21,13 +23,15 @@ def webhook():
     return "!", 200
 
 
+# Комманда "/start"
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
+# Отправка изображения аккорда
 @bot.message_handler(content_types=["text"])
-def send_photo(message):
+def send_chords(message):
     try:
         image = open('res/chords/' + message.text.upper() + '.jpg', 'rb')
         bot.send_photo(message.chat.id, image)
@@ -36,5 +40,5 @@ def send_photo(message):
         bot.send_message(message.chat.id, 'В нашей базе нет такого аккорда')
 
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  # Запуск сервера
+server = Flask(__name__)  # Наверно это нужно, чтобы Heroku не засыпал
