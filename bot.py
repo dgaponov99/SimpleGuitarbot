46f6d2bd6ffc3b9c5ffcd2c808b3836d55779977
dmitry <dgaponov99@mail.ru>
2017-12-07 02:41:20 +0300

    try fix bag

--- bot.py
+++ bot.py
@@ -107,7 +107,10 @@ def send_chords(message):
                 chords_db.set_files_id(message.text.lower(), ids, caption)
             else:
                 keyboard = types.InlineKeyboardMarkup()
-                keyboard.add(types.InlineKeyboardButton(text=string_values.to_offer, callback_data=str(message.text)))
+                keyboard.add(types.InlineKeyboardButton(text=string_values.to_offer,
+                                                        callback_data=string_values.message_to_admins.format(
+                                                            message.from_user.first_name, message.chat.id, message.text)
+                                                        ))
                 bot.send_message(message.chat.id, string_values.text_inline_button, reply_markup=keyboard)
         else:
             for chord_file_id in chord_files_id:
