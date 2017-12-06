a263c3cb3a9d554e4516da04b2749735d13a1dc6
dmitry <dgaponov99@mail.ru>
2017-12-06 23:08:39 +0300

    try in sync manner

--- chords_db.py
+++ chords_db.py
@@ -9,9 +9,9 @@ db = client.test
 collection = db['chords']
 
 
-async def get_files_id(chord):
+def get_files_id(chord):
     global collection
-    return await collection.find_one({'chord': chord})['files_id']
+    return collection.find_one({'chord': chord})['files_id']
 
 
 def set_files_id(chord, files_id):
