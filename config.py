655539021245ce3028e33468321421d9f2aea8eb
dmitry <dgaponov99@mail.ru>
2017-12-23 15:39:24 +0300

    new app heroku

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Параметры бота"""
 
 TOKEN = '442370610:AAHZ0eYC7Bx9Qsm9qNIaJJgK-LKUgmxjUj4'
-URL = 'https://quiet-springs-54074.herokuapp.com/' + TOKEN
+URL = 'https://vast-plains-73414.herokuapp.com/' + TOKEN
 LINK = 'mongodb://dgaponov99:dg99561421@simpleguitarbot-shard-00-00-ze8qe.mongodb.net:27017,' \
        + 'simpleguitarbot-shard-00-01-ze8qe.mongodb.net:27017,simpleguitarbot-shard-00-02-ze8qe.mongodb.net:27017' \
        + '/test?ssl=true&replicaSet=SimpleGuitarbot-shard-0&authSource=admin'
