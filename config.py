fde3a247aa7bf82460c99b5792f71f326199f06a
dmitry <dgaponov99@mail.ru>
2017-12-23 16:40:56 +0300

    try

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Параметры бота"""
 
 TOKEN = '442370610:AAHZ0eYC7Bx9Qsm9qNIaJJgK-LKUgmxjUj4'
-URL = 'https://nameless-shelf-98915.herokuapp.com/' + TOKEN
+URL = 'https://immense-earth-21017.herokuapp.com/' + TOKEN
 LINK = 'mongodb://dgaponov99:dg99561421@simpleguitarbot-shard-00-00-ze8qe.mongodb.net:27017,' \
        + 'simpleguitarbot-shard-00-01-ze8qe.mongodb.net:27017,simpleguitarbot-shard-00-02-ze8qe.mongodb.net:27017' \
        + '/test?ssl=true&replicaSet=SimpleGuitarbot-shard-0&authSource=admin'
